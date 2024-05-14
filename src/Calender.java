c4e85c0e847d0cb563daf67b86d9b9086a1a5080
OneLittleSpider <25jjian@athenian.org>
2024-05-13 20:47:14 -0700

    5.13

--- src/Calender.java
+++ src/Calender.java
@@ -1,24 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Calender {
 
     JFrame frame;
-    JPanel DatePanel;
+    JPanel monthPanel;
 
     public Calender(){
         frame = new JFrame("Calender");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.pack();
+        JPanel monthPanel = new JPanel();
+        for(int i=0;i<5;i++){
+            JLabel dayLabel = new JLabel("this is a panel"+i);
+            JTextField textField = new JTextField(10 );
+            String text = textField.getText();
+            boolean selected = false;
+            JCheckBox checkBox = new JCheckBox(text, selected);
+            Day day = new Day(dayLabel, textField, checkBox);
+            monthPanel.add(day);
+        }
+
+
+        frame.add(monthPanel);
+
+        frame.setSize(500, 500);
         frame.setVisible(true);
+
 
     }
 
+
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         Calender x = new Calender();
     }
 }
---
+++ src/Day.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+public class Day extends JPanel{
+    private JLabel day;
+    private JTextField textField;
+    private JCheckBox checkBox;
+
+        public Day(JLabel day, JTextField textField, JCheckBox checkBox) {
+        this.day=day;
+        this.textField=textField;
+        this.checkBox=checkBox;
+
+
+        }
+    }
+
